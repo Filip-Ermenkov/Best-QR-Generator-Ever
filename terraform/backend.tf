6dc4ac7a828fe5815b4c703dcc44ee3e07b85865
Filip05-TUES <f.ermenkov@gmail.com>
2025-12-30 12:31:13 +0200

    Set up CI/CD for Terraform and initialize it

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "devops-capstone-terraform-state"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
