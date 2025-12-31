d9b4a5b0038bd660ce8cc0aee872635c71d4c7cf
Filip05-TUES <f.ermenkov@gmail.com>
2025-12-31 19:41:39 +0200

    Change terraform state bucket name

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devops-capstone-terraform-state"
+    bucket         = "best-qr-ever-terraform-state"
     key            = "state/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
