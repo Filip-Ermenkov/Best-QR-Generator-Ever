d3eef52714262e67283327a2795894b3e8811f19
Filip05-TUES <f.ermenkov@gmail.com>
2026-01-24 12:05:07 +0200

    Remove annotations as they are not needed
    EKS Pod Identity Association is used instead

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -2,9 +2,6 @@ resource "kubernetes_service_account" "aws_lbc" {
   metadata {
     name      = "aws-load-balancer-controller"
     namespace = "kube-system"
-    annotations = {
-      "eks.amazonaws.com/role-arn" = aws_iam_role.alb_controller.arn
-    }
   }
 }
 
