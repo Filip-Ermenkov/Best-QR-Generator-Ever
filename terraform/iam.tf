5b1c491e898e92f94b32838e573d2bdc7aa695e6
Filip05-TUES <f.ermenkov@gmail.com>
2026-01-27 15:29:16 +0200

    Remove redundancies

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -35,8 +35,7 @@ resource "aws_iam_role_policy_attachment" "node_policies" {
   for_each = toset([
     "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
     "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy",
-    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
-    "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+    "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   ])
   policy_arn = each.value
   role       = aws_iam_role.eks_nodes.name
--- terraform/security-groups.tf
+++ terraform/security-groups.tf
@@ -66,13 +66,3 @@ resource "aws_security_group" "eks_nodes" {
     "kubernetes.io/cluster/${var.project_name}" = "owned"
   }
 }
-
-resource "aws_security_group_rule" "cluster_ingress_node_https" {
-  description              = "Allow nodes to communicate with the cluster API Server"
-  type                     = "ingress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
-  source_security_group_id = aws_security_group.eks_nodes.id
-}
